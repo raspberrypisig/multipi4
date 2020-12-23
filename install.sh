7ea1ec0da772336bec1e24d78190c92b0e7dc899
Mohan Gupta <raspberrypisig@gmail.com>
2020-12-23 22:56:01 +1100

    add install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo apt install -y yad git
+
+mkdir -p /tmp/multipi4
+git clone https://github.com/raspberrypisig/multipi4
+cd multipi4
+cp multipi4* /usr/local/bin
+sudo rsync -av etc/ /etc
+
+mkdir -p ~/.local/share/applications
+APP=/usr/local/bin/multipi4-gui
+ICON=/usr/local/share/multipi4.png
+
+cp multipi4.png $ICON
+
+echo "[Desktop Entry]
+Name=MultiPi4
+Comment=MultiBoot Raspberry Pi 4
+Exec=${APP}
+Icon=${ICON}
+Terminal=false
+Type=Application
+Categories=Utility;" > ~/.local/share/applications/pi-apps.desktop
+
