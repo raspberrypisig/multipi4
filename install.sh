8796c08f497ad0e3920712466d10441069f8174a
Mohan Gupta <raspberrypisig@gmail.com>
2020-12-28 09:55:36 +1100

    install btrfs-progs in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 sudo apt update
-sudo apt install -y yad git gnome-terminal
+sudo apt install -y yad git gnome-terminal btrfs-progs
 
 rm -rf /tmp/multipi4
 mkdir -p /tmp/multipi4
