b605edd2bbebb40927afabfa07acc55213401fcd
raspberrypisig <raspberrypisig@gmail.com>
2026-03-30 19:37:51 +1100

    minor changes

--- etc/multipi4/raspbian64/setup.sh
+++ etc/multipi4/raspbian64/setup.sh
@@ -3,6 +3,8 @@ set -x
 
 volname="$1"
 
+echo "MODULES=most" > /etc/initramfs-tools/conf.d/multipi4.conf
+
 apt update
 apt install -y initramfs-tools btrfs-progs
 
@@ -14,25 +16,25 @@ echo btrfs >> /etc/initramfs-tools/modules
 #mkinitramfs -o /boot/initramfs-btrfs -v $VERSION
 update-initramfs -u -k all
 
-if [ -d "/boot/firmware" ]; then
-    BOOT_DIR="/boot/firmware"
+# if [ -d "/boot/firmware" ]; then
+#     BOOT_DIR="/boot/firmware"
     
-    # Raspberry Pi OS auto-generates initramfs8 (for Pi4) or initramfs_2712 (for Pi5)
-    # We copy it to match what MultiPi4's config.txt expects.
-    if [ -f "$BOOT_DIR/initramfs8" ]; then
-        cp "$BOOT_DIR/initramfs8" "$BOOT_DIR/initramfs-btrfs"
-    elif [ -f "$BOOT_DIR/initramfs_2712" ]; then
-        cp "$BOOT_DIR/initramfs_2712" "$BOOT_DIR/initramfs-btrfs"
-    else
-        # Fallback just in case
-        LATEST_INITRD=$(ls -S /boot/initrd.img-* | head -n 1)
-        cp "$LATEST_INITRD" "$BOOT_DIR/initramfs-btrfs"
-    fi
-else
-    BOOT_DIR="/boot"
-    LATEST_INITRD=$(ls -S /boot/initrd.img-* | head -n 1)
-    cp "$LATEST_INITRD" "$BOOT_DIR/initramfs-btrfs"
-fi
+#     # Raspberry Pi OS auto-generates initramfs8 (for Pi4) or initramfs_2712 (for Pi5)
+#     # We copy it to match what MultiPi4's config.txt expects.
+#     if [ -f "$BOOT_DIR/initramfs8" ]; then
+#         cp "$BOOT_DIR/initramfs8" "$BOOT_DIR/initramfs-btrfs"
+#     elif [ -f "$BOOT_DIR/initramfs_2712" ]; then
+#         cp "$BOOT_DIR/initramfs_2712" "$BOOT_DIR/initramfs-btrfs"
+#     else
+#         # Fallback just in case
+#         LATEST_INITRD=$(ls -S /boot/initrd.img-* | head -n 1)
+#         cp "$LATEST_INITRD" "$BOOT_DIR/initramfs-btrfs"
+#     fi
+# else
+#     BOOT_DIR="/boot"
+#     LATEST_INITRD=$(ls -S /boot/initrd.img-* | head -n 1)
+#     cp "$LATEST_INITRD" "$BOOT_DIR/initramfs-btrfs"
+# fi
 
 
 sed -i "s/PLACEHOLDER/$volname/" /boot/cmdline.txt 
