7aaa3894cc12aaec977d19f52a2b9610291b5cf6
raspberrypisig <raspberrypisig@gmail.com>
2026-04-01 09:26:12 +1100

    ubuntu: making minor changes

--- etc/multipi4/ubuntu/setup.sh
+++ etc/multipi4/ubuntu/setup.sh
@@ -13,27 +13,17 @@ touch /etc/cloud/cloud-init.disabled
 
 echo btrfs >> /etc/initramfs-tools/modules
 
-VERSION=$(find /lib/modules -name '*-raspi' -exec basename {} \; )
 echo "RESUME=none" > /etc/initramfs-tools/conf.d/resume
-update-initramfs -c -k $VERSION
-cp /boot/initrd.img-$VERSION /boot/firmware/initrd.img
+update-initramfs -c -k all
 
 sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/cmdline.txt 
 sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
 
-#mv /etc/resolv.conf.old /etc/resolv.conf
-
-#systemctl disable sssd
-
-apt-mark hold linux-headers-$VERSION linux-modules-$VERSION linux-image-$VERSION
-
-#echo -e "ubuntu\nubuntu\n" | passwd ubuntu
-#apt install -y network-manager
 useradd -G sudo -s /bin/bash -d /home/pi -m pi
-chown pi:pi /home/pi
-echo -e "raspberry\nraspberry\n" | passwd pi
-useradd -G sudo -s /bin/bash -d /home/pi -m ubuntu
-chown ubuntu:ubuntu /home/ubuntu
-echo -e "ubuntu\nubuntu\n" | passwd ubuntu
-#chpasswd <<< "ubuntu:ubuntu"
+echo "pi:raspberry" | chpasswd
+echo "pi ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/010_pi-nopasswd
+chmod 0440 /etc/sudoers.d/010_pi-nopasswd
+
+echo "ubuntu:ubuntu" | chpasswd
+
 
