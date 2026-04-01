546a79d57ee4e50b73bd9e1395863f2a9c6dd39f
raspberrypisig <raspberrypisig@gmail.com>
2026-04-01 11:45:33 +1100

    ubuntu changes

--- etc/multipi4/ubuntu/setup.sh
+++ etc/multipi4/ubuntu/setup.sh
@@ -3,12 +3,6 @@ set -x
 
 volname="$1"
 
-#mv /etc/resolv.conf /etc/resolv.conf.old
-#echo nameserver 8.8.8.8 > /etc/resolv.conf
-
-#apt update
-#apt install -y initramfs-tools btrfs-progs
-
 touch /etc/cloud/cloud-init.disabled
 
 echo btrfs >> /etc/initramfs-tools/modules
@@ -16,14 +10,17 @@ echo btrfs >> /etc/initramfs-tools/modules
 echo "RESUME=none" > /etc/initramfs-tools/conf.d/resume
 update-initramfs -c -k all
 
-sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/cmdline.txt 
+if [ -f /boot/firmware/current/cmdline.txt ]; then
+  sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/current/cmdline.txt
+else
+  sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/cmdline.txt
+fi
+
+#sed -i "s/PLACEHOLDER/$volname/" /boot/firmware/cmdline.txt 
 sed -i "s/PLACEHOLDER/$volname/" /etc/fstab
 
 useradd -G sudo -s /bin/bash -d /home/pi -m pi
 echo "pi:raspberry" | chpasswd
 echo "pi ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/010_pi-nopasswd
 chmod 0440 /etc/sudoers.d/010_pi-nopasswd
-
-echo "ubuntu:ubuntu" | chpasswd
-
 
